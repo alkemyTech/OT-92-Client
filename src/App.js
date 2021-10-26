810745791766f5f99d38370881c3d074008583af
Gabriel Alabí <ivangabrielalabi@gmail.com>
2021-10-26 12:12:01 -0300

    activity details component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route path="/actividades/:id" component={ActivitiesDetail} />
+          <Route path="/activities/:id" component={ActivitiesDetail} />
         </Switch>
       </BrowserRouter>
     <div className="App">
