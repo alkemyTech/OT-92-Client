d3f6bde2071704f256dc45a601fe2655f17b60a5
Gabriel Alabí <ivangabrielalabi@gmail.com>
2021-10-26 01:16:56 -0300

    Activities Detail added v1

---
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import './ActivitiesDetail.css';
+const ActivitiesDetail = () => {
+  let { id: paramsId } = useParams();
+  const activities = [
+    {
+      id: "1",
+      title: "Recorrido por el comedor",
+      body: "se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna",
+    },
+    {
+      id: "2",
+      title: "Visitas clases de ingles",
+      body: "se visita a los alumnos de primaria en sus clases de ingles ",
+    },
+    {
+      id: "3",
+      title: "torneo de futbol 5 ",
+      body: "los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo",
+    },
+  ];
+  const paramsActivity = activities.filter(
+    (activities) => activities.id === paramsId
+  );
+  return (
+    <>
+      <div>
+        {paramsActivity[0] ? (
+          <div>
+            <h1 className=" h1 text-center Activities-title">{paramsActivity[0].title}</h1>
+            <h1 className="h3 Activities-body">{paramsActivity[0].body}</h1>
+          </div>
+        ) : (
+          <div>
+            <h1 className="Activities-title">Oh Oh! hemos tenido problemas con esa actividad :( </h1>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ActivitiesDetail;
