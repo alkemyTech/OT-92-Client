1e6d007815a0091a578c2752a4698dd2e220b3fe
Gabriel Alabí <ivangabrielalabi@gmail.com>
2021-10-27 11:04:01 -0300

    pr changes

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -6,17 +6,17 @@ const ActivitiesDetail = () => {
     {
       id: "1",
       title: "Recorrido por el comedor",
-      body: "se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna",
+      body: "se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna.",
     },
     {
       id: "2",
       title: "Visitas clases de ingles",
-      body: "se visita a los alumnos de primaria en sus clases de ingles ",
+      body: "se visita a los alumnos de primaria en sus clases de ingles. ",
     },
     {
       id: "3",
       title: "torneo de futbol 5 ",
-      body: "los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo",
+      body: "los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo.",
     },
   ];
   const paramsActivity = activities.filter(
@@ -26,13 +26,13 @@ const ActivitiesDetail = () => {
     <>
       <div>
         {paramsActivity[0] ? (
-          <div>
-            <h1 className=" h1 text-center Activities-title">{paramsActivity[0].title}</h1>
-            <h1 className="h3 Activities-body">{paramsActivity[0].body}</h1>
+          <div className="container d-flex flex-column justify-items-center mt-5">
+            <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
+            <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1>
           </div>
         ) : (
-          <div>
-            <h1 className="Activities-title">Oh Oh! hemos tenido problemas con esa actividad :( </h1>
+          <div className="mt-5 container">
+            <h1 className="Activities-title ">Oh Oh! hemos tenido problemas con esa actividad :( </h1>
           </div>
         )}
       </div>
