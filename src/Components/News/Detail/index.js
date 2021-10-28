3b5333617a608f524e3b1bff383c6090033c5142
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-10-28 13:32:39 -0300

    Revert "Commented some functions"
    This reverts commit b77652e4c06493dd316ab30678eedc426f1704a8.

--- src/Components/News/Detail/index.js
+++ src/Components/News/Detail/index.js
@@ -1,17 +1,17 @@
 import { useParams } from "react-router-dom";
-import './index.css'
+
 
 const NewsDetail = () => {
-  let { id: paramsId } = useParams(); // utilizamos useParams para acceder a los parámetros de una ruta determinada
-  const news = [                     // creamos un array de objetos con los datos que queremos mostrar
+  let { id: paramsId } = useParams();
+  const news = [
     {
       id: "1",
-      content: "Se realizó una nueva edición del show del chiste en La Cava, voluntarios de la comunidad se animaron a compartir chistes y realizar trucos de magia para los más chicos.",
+      content: "Se realizo una nueva edición del show del chiste en La Cava, voluntarios de la comunidad se animaron a compartir chistes y realizar trucos de magia para los más chicos.",
       image: "http://ongapi.alkemy.org/storage/m7c2U9zdM6.png"
     },
     {
       id: "2",
-      content: "La organización cuenta con profesionales de educación física que realizan actividades recreativas con chicos de todas las edades.",
+      content: "La organizaci&oacute;n cuenta con profesionales de educación física que realizan actividades recreativas con chicos de todas las edades.",
       image: "http://ongapi.alkemy.org/storage/ZVyW7MoXH3.jpeg"
     },
     {
@@ -21,33 +21,23 @@ const NewsDetail = () => {
     }
   ]
   
-  const paramsNews = news.filter(  // utilizamos el array method 'filter' para filtrar los datos que queremos mostrar
+  const paramsNews = news.filter(
     (news) => news.id === paramsId
   );
-  
   return (
     <>
       <div>
-        {paramsNews[0] 
-        
-            ? /* mostramos los datos a través de una ternaria */
-            
-            (
-            <div>
-            {/* <h1>{paramsActivity[0].title}</h1> */}
-                <h3>{paramsNews[0].content}</h3>        
-                <img src={paramsNews[0].image} />
-            </div>
-            )
-            
-            : {/* no mostramos los datos si no se encuentra el id */}
-            
-            (
-            <div>
-                <h1>No hemos podido encontrar lo que estas buscando</h1> 
-            </div>
-            )
-        }
+        {paramsNews[0] ? (
+          <div>
+           {/* <h1>{paramsActivity[0].title}</h1> */}
+            <h1>{paramsNews[0].content}</h1>
+            <img src={image} />
+          </div>
+        ) : (
+          <div>
+            <h1>No hemos podido encontrar lo que estas buscando</h1>
+          </div>
+        )}
       </div>
     </>
   );
