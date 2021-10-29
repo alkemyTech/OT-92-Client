c2856aa8870ed1ddb53128dd52b52d10ad830838
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-10-29 16:17:42 -0300

    Updated Route
    Renamed a route in "App.js"

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
-          <Route path="/testimonials/:id" component={TestimonialForm} />
+          <Route path="/create-testimonials/:id" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-project" component={ProjectsForm} />
