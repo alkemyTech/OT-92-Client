39daec2bd734ffd09c32dd5eabc608ccfc03e1e4
fdelavalle <delavallef4@gmail.com>
2021-10-29 09:58:40 -0300

    added formik and yup validations to the form

--- src/Components/Organization/UdpateDataForm.js
+++ src/Components/Organization/UdpateDataForm.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import '../FormStyles.css';
-
-//se puede hacer con formik esto? preguntar
 
 /*
 Criterios de aceptacion: Al ingresar a la ruta /backoffice/organization/edit se mostará el formulario para editar los campos name, logo, shortDescription, longDescription y links de redes sociales. El campo shortDescription debe poder editarse con CKEditor. Todos los campos son obligatorios, y deben validarse al hacer submit. En el caso de que todos estén completados, dejar el método vacío, ya que posteriormente se realizará una petición a la API
@@ -20,11 +22,11 @@ const UpdateDataForm = (props) => {
   //const fileInputRef = useRef()
   const [image, setImage] = useState();
   const [imagePreview, setImagePreview] = useState();
+  const [shortDescription, setShortDescription] = useState();
 
   useEffect(() => {
     if (image) {
       const reader = new FileReader();
-      console.log(image);
       reader.onload = () => {
         setImagePreview(reader.result);
       };
@@ -34,60 +36,125 @@ const UpdateDataForm = (props) => {
     }
   }, [image]);
 
+  const onEditorChange = (event, editor) => {
+    const data = editor.getData();
+    //setShortDescription(data);
+    formik.values.shortDescription = data;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: props.organization ? props.organization.name : '',
+      logo: props.organization ? props.organization.logo : '',
+      shortDescription: props.organization
+        ? props.organization.shortDescription
+        : '',
+      longDescription: props.organization
+        ? props.organization.longDescription
+        : '',
+      linkInstagram: props.organization ? props.organization.linkInstagram : '',
+      linkFacebook: props.organization ? props.organization.linkFacebook : '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('El nombre es obligatorio'),
+      logo: Yup.string().required('El logo es obligatorio'),
+      shortDescription: Yup.string().required(
+        'La descripción corta es obligatoria'
+      ),
+      longDescription: Yup.string().required(
+        'La descripción larga es obligatoria'
+      ),
+      linkInstagram: Yup.string().url('El link debe ser una URL válida'),
+      linkFacebook: Yup.string().url('El link debe ser una URL válida'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div className='mt-5'>
       <h1 className='text-center'>Update Data Form</h1>
-      <form action='' className='form-container mt-5 form-group'>
+      <form
+        onSubmit={formik.handleSubmit}
+        className='form-container form-group'
+      >
         <label htmlFor='name' className=''>
           Name
         </label>
-        <input type='text' name='name' id='name' className='form-control' />
-        <label htmlFor='logo'>Logo</label>
-        <input
-          type='file'
-          accept='image/png, image/jpeg'
-          name='logo'
-          id='logo'
-          className='form-control'
-          onChange={(event) => {
-            const file = event.target.files[0];
-            if (file && file.type.substring(0, 5) === 'image') {
-              setImage(file);
-            } else {
-              setImage(null);
-            }
-          }}
-        />
-        {imagePreview && <img src={imagePreview} alt='organization logo' />}
-
-        <label htmlFor='shortDescription'>Short Description</label>
         <input
           type='text'
+          name='name'
+          id='name'
+          className='form-control mt-2'
+          value={formik.values.name}
+          onChange={formik.handleChange}
+        />
+        <label htmlFor='logo'>Logo</label>
+        <div className='d-flex justify-content-between'>
+          <input
+            style={image && { height: '2.4rem', width: '55rem' }}
+            type='file'
+            accept='image/png, image/jpeg'
+            name='logo'
+            id='logo'
+            className='form-control'
+            onChange={(event) => {
+              const file = event.target.files[0];
+              if (file && file.type.substring(0, 5) === 'image') {
+                setImage(file);
+                formik.values.logo = file;
+              } else {
+                setImage(null);
+              }
+            }}
+          />
+          {imagePreview && (
+            <img
+              src={imagePreview}
+              alt='organization logo'
+              style={{ width: '200px', height: '100px' }}
+            />
+          )}
+        </div>
+        <label>Short Description</label>
+        <CKEditor
           name='shortDescription'
           id='shortDescription'
-          className='form-control'
+          editor={ClassicEditor}
+          data={shortDescription}
+          onChange={onEditorChange}
         />
+
         <label htmlFor='longDescription'>Long Description</label>
         <input
           type='text'
           name='longDescription'
           id='longDescription'
           className='form-control'
+          value={formik.values.longDescription}
+          onChange={formik.handleChange}
         />
-        <label htmlFor='links'>Link de Instagram</label>
+        <label htmlFor='linkInstragram'>Link de Instagram</label>
         <input
           type='text'
           name='linkInstagram'
           id='linkInstagram'
           className='form-control'
+          value={formik.values.linkInstagram}
+          onChange={formik.handleChange}
         />
-        <label htmlFor='links'>Link de Facebook</label>
+        <label htmlFor='linkFacebook'>Link de Facebook</label>
         <input
           type='text'
           name='linkFacebook'
-          id='linkInstagram'
+          id='linkFacebook'
           className='form-control'
+          value={formik.values.linkFacebook}
+          onChange={formik.handleChange}
         />
+
+        {shortDescription && <h1> {shortDescription} </h1>}
         <button type='submit' className='btn btn-primary'>
           Submit
         </button>
