e4d6a2b5f3b4b49e894ac6daa15d54ff82a01401
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-10-28 21:43:42 -0300

    Added functions to handle change and submit
    With the "handleSubmit" function the user will be able to create or edit a testimonial through the form.

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -14,4 +14,46 @@ export default function TestimonialsForm () {
     const editTestimonial = `http://ongapi.alkemy.org/public/api/testimonials/${id}`
     const createTestimonial = 'http://ongapi.alkemy.org/api/testimonials'
     const [initialValues, setInitialValues] = useState(testimonialData);
+
+    const handleChange = (e) => { 
+      if (e.target.name === 'title') {
+          setInitialValues({ ...initialValues, title: e.target.value })
+      } 
+    }
+  
+    const handleSubmit = (e) => { 
+      
+      if (!id) {
+        axios.post(createTestimonial, {
+            name: initialValues.title,
+            image: initialValues.image,
+            content: initialValues.content
+            
+        }).then(res => {
+            if (res.status === 200) {
+                alert('El testimonio se creó correctamente');
+                return setInitialValues(testimonialData)
+            }
+        }).catch(err => {
+            alert(err)
+        })
+      }
+    
+      if (id) {
+          axios.put(editTestimonial, {
+              name: initialValues.title,
+              image: initialValues.image,
+              content: initialValues.content
+          }).then(res => {
+              if (res.status === 200) {
+                  alert("El testimonio se actualizó correctamente");
+                  return setInitialValues(testimonialData)
+              }
+          }).catch(err => {
+              alert(err)
+          })
+      }
+      e.preventDefault();
+      console.log(initialValues);
+    }
 }
