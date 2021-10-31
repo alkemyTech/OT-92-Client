9b72e4f151c4a28b03407c287166367857ff5fea
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-10-31 16:10:52 -0300

    fetch data implementation

--- src/Components/Organization/Organization.jsx
+++ src/Components/Organization/Organization.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const Organization = () => {
-    return (
-        <div>
-            <h1>Organization</h1>
-        </div>
-    )
-}
+  const [serverError, setServerError] = useState(null);
+  const [data, setData] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios({
+          method: 'GET',
+          url: 'http://ongapi.alkemy.org/public/api/organization',
+        });
+        setData(response.data.data);
+      } catch {
+        setServerError(
+          'Algo salió mal. Por favor, actualice la página e intente de nuevo.'
+        );
+      }
+    };
 
-export default Organization
+    getData();
+  }, []);
+
+//   console.log(data);
+  return (
+    <div>
+      <h1>Organization</h1>
+      {serverError ? <p>{serverError}</p> : null}
+      {data ? <p>{data.name}</p> : <p>Cargando...</p>}
+    </div>
+  );
+};
+
+export default Organization;
