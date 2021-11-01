3abe1d3426204de85fe83734c86ef986d17d9325
Gabriel <ivangabrielalabi@gmail.com>
2021-10-31 23:16:45 -0300

    news cards and noNews message fix

--- src/Components/News/Detail/NewsFormat.js
+++ src/Components/News/Detail/NewsFormat.js
@@ -1,25 +1,25 @@
-import './NewsFormat.css';
-const NewsFormat = ({content}) => {
-    console.log(content)
-    const {title, image} = content;
-    return ( 
-        <>
-        <div className="w-50  customize-card mx-3 ">
-        <img className="customize-image-border card-img-top" src={image} 
+import "./NewsFormat.css";
+const NewsFormat = ({ content }) => {
+  const { title, image } = content;
+  return (
+    <>
+      <div className="customize-card  card-display d-block mx-auto mb-3 ">
+        <img
+          className="customize-image-border card-image-top "
+          src={image}
           onError={(e) => {
             e.target.onerror = null;
             e.target.src = "/error image somos mas.jpg";
           }}
-        
-        
-        alt="no image" />
-        <h1 className="h3 pl-4 pt-2 w-100 cursor-default card-title">{title}</h1>
+          alt="no image"
+        />
+        <h1 className="h3 pl-4 pt-2 w-100 cursor-default card-title pr-2">
+          {title}
+        </h1>
         <h3 className="bottom-text"> Leer mas </h3>
-        
-        </div>
-        
-        </>
-     );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default NewsFormat;
