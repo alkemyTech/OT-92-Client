3887a80fddf1ceac9f32f20ea252ae1587de17c4
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-01 12:35:33 -0300

    Updated ScreenDashboard.js
    Updated welcoming message.

--- src/Components/Backoffice/ScreenDashboard.js
+++ src/Components/Backoffice/ScreenDashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ScreenDashboard() {
     return (
         <div>
-            <p>Bienvenido al backoffice</p>
+            <p>Bienvenido al backoffice.</p>
         </div>
     )
 }
