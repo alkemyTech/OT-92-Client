a810dea07cc9616abd8cffa19e392ad2e374c6d8
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-03 15:01:34 -0300

    UserList table and styles

--- src/Components/Backoffice/Users/UsersList.js
+++ src/Components/Backoffice/Users/UsersList.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const UsersList = () => {
+const UsersList = ({ usersData }) => {
+
     return (
-        <div>
-            
+        <div className="table-responsive">
+            <table className="table table-striped table-hover" id="tableResponsive">
+                <thead>
+                    <tr>
+                        <th scope="col">#ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        usersData.map(user => (
+                            <tr key={user.id}>
+                                <th scope="row">{user.id}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    <p>Botones</p>
+                                    {/* service PUT con user.id como parametro */}
+                                    {/* service DELETE con user.id como parametro */}
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
