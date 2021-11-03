b1c3f1fc5b0cc6129b16b29dd3dad33ea8c5443a
tankez0r <martintorres1992@gmail.com>
2021-11-03 17:30:43 -0300

    posible metodo, falta consensuar con equipo canon de servicios

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
+
 const config = {
     headers: {
         Group: 92                //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
 
-export default Get
+const queryGetData = async (section) => {
+    const url = `http://ongapi.alkemy.org/api/${section}`
+    let res = await axios.get(url, config);
+    try {
+        return res.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+export { queryGetData };
