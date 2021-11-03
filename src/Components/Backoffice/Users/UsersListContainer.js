ad7dd1fe451f68574e6f1bfc69d6c4067436c528
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-03 14:59:11 -0300

    Corrections in ListContainer

--- src/Components/Backoffice/Users/UsersListContainer.js
+++ src/Components/Backoffice/Users/UsersListContainer.js
@@ -5,18 +5,17 @@ import UsersList from './UsersList'
 const UsersListContainer = () => {
     /* Aquí ejecutariamos el service para obtener los usuarios y seteariamos esa info en un state o context*/
 
-    const mockData = [
-        {"id":354,"name":"Test","email":"test@alkemy.com"},
-        {"id":355,"name":"testy1","email":"testy@mail.com"},
-        {"id":356,"name":"testy2","email":"test@mail.com"}
+    const usersData = [
+        {id:354, name:"Test", email:"test@alkemy.com"},
+        {id:355, name:"testy1", email:"testy@mail.com"},
+        {id:356, name:"testy2", email:"test@mail.com"}
     ] /* Borrar este array una vez integrado el service para peticion GET. */
-
     return (
         <div className="container">
             <div id="usersListContainer">
                 <h3 className="headers">Lista de usuarios:</h3>
                 <UsersList 
-                    usersData={ mockData } /* Cambiar el nombre de la prop una vez integrado el service */
+                    usersData={ usersData } /* Cambiar el nombre de "mockData" una vez integrado el service */
                 />
             </div>
         </div>
