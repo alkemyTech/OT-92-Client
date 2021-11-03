e3d864117051303fbb65f2bdce1d1fbf4c6e1473
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-03 17:56:58 -0300

    Buttons functionality and passing Id and Action params to the function

--- src/Components/Backoffice/Users/UsersList.js
+++ src/Components/Backoffice/Users/UsersList.js
@@ -3,6 +3,16 @@ import { IoTrashSharp, IoPencilSharp } from "react-icons/io5";
 
 const UsersList = ({ usersData }) => {
 
+    const handleAction = (action, id) => {
+        if (action === "edit"){
+            /* Aquí va la funcion para editar con el ID correspondiente */
+            alert(`El usuario con ID: ${id} ha sido editado`)
+        } if(action === "delete"){
+            /* Aquí va la funcion para eliminar con el ID correspondiente */
+            alert(`El usuario con ID: ${id} ha sido eliminado`)
+        }
+    }
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-hover" id="tableResponsive">
@@ -23,10 +33,18 @@ const UsersList = ({ usersData }) => {
                                 <td>{user.email}</td>
                                 <td>
                                     <div className="actions_container">
-                                        <button type="button" className="btn btn-info btn-sm edit_button">
+                                        <button 
+                                            type="button" 
+                                            className="btn btn-info btn-sm edit_button"
+                                            onClick={() => handleAction("edit", user.id)}
+                                        >
                                             <IoPencilSharp />
                                         </button>{' '}
-                                        <button type="button" className="btn btn-danger btn-sm delete_button">
+                                        <button 
+                                            type="button" 
+                                            className="btn btn-danger btn-sm delete_button"
+                                            onClick={() => handleAction("delete", user.id)}
+                                        >
                                             <IoTrashSharp />
                                         </button>
                                     </div>
