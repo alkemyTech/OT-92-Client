e72c5d95717f6214b08c3cc9828d224fbff2ad15
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-11-03 14:21:42 -0300

    slider and welcome text placeholders implemented

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from 'react';
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return (
+    <div className='d-flex flex-column'>
+      <div className='d-flex bg-primary align-items-center justify-content-center' style={{height: '50vh'}}>
+        <h1>Slider</h1>
+      </div>
+      <h1 className='m-3'>Texto de bienvenida</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
