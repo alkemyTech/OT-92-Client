d1c1a65298833cf3160745ddde58952c656f6cf8
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-03 13:46:46 -0300

    Users list title, UserList component imported and styles

--- src/Components/Backoffice/Users/UsersListContainer.js
+++ src/Components/Backoffice/Users/UsersListContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import UsersList from './UsersList'
 
 const UsersListContainer = () => {
     return (
-        <div>
-            
+        <div className="container">
+            <div id="usersListContainer">
+                <h3 className="headers">Lista de usuarios:</h3>
+                <UsersList />
+            </div>
         </div>
     )
 }
