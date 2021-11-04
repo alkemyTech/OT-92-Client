f65dcc50a902df814ca76d08c04d7cbd12dd3078
fdelavalle <80860340+fdelavalle@users.noreply.github.com>
2021-11-04 16:57:56 -0300

    Updated ActivitiesDetail.js

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -1,32 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import './ActivitiesDetail.css';
-<<<<<<< HEAD
 import { activitiesService } from '../../../Services/privateApiService';
-=======
 import ActivitiesDetailContent from "./ActivitiesDetailContent";
 
->>>>>>> main
 const ActivitiesDetail = () => {
   let { id: paramsId } = useParams();
 
   const activities = [
     {
-<<<<<<< HEAD
-      id: '1',
-      title: 'Recorrido por el comedor',
-      body: 'se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna.',
-    },
-    {
-      id: '2',
-      title: 'Visitas clases de ingles',
-      body: 'se visita a los alumnos de primaria en sus clases de ingles. ',
-    },
-    {
-      id: '3',
-      title: 'torneo de futbol 5 ',
-      body: 'los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo.',
-=======
       id: "1",
       title: "Recorrido por el comedor",
       body: "<p>Se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna.</p>",
@@ -40,7 +22,6 @@ const ActivitiesDetail = () => {
       id: "3",
       title: "Torneo de futbol 5 ",
       body: "<p>Los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo.</p>",
->>>>>>> main
     },
   ];
   const paramsActivity = activities.filter(
@@ -50,15 +31,6 @@ const ActivitiesDetail = () => {
     <>
       <div>
         {paramsActivity[0] ? (
-<<<<<<< HEAD
-          <div className='container d-flex flex-column justify-items-center mt-5'>
-            <h1 className=' h1 w-100 text-center Activities-title'>
-              {paramsActivity[0].title}
-            </h1>
-            <h1 className='h3 Activities-body text-justify'>
-              {paramsActivity[0].body}
-            </h1>
-=======
           <div className="container d-flex flex-column justify-items-center mt-5">
             <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
            {/* Anterior contenido sin mi componente  <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1> */}
@@ -66,7 +38,6 @@ const ActivitiesDetail = () => {
               body={paramsActivity[0].body}
               paramsId={paramsId}
             />
->>>>>>> main
           </div>
         ) : (
           <div className='mt-5 container'>
