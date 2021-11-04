3833f3f9b93301c4afe912a619e1c6cb7a2a46bd
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-04 13:42:36 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,47 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import ActivitiesForm from "./Components/Activities/ActivitiesForm";
 import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import ScreenDashboard from "./Components/Backoffice/ScreenDashboard";
-import NewsDetail from "./Components/News/Detail";
-import Organization from "./Components/Organization/Organization";
-import MembersCreateEdit from "./Components/Members/MembersCreateEdit";
-import UpdateDataForm from "./Components/Organization/UdpateDataForm";
-import ActivitiesDetail from "./Components/Activities/Detail/ActivitiesDetail";
-import HomeForm from "./Components/Home/HomeForm";
-import NewsDisplay from "./Components/News/NewsDisplay";
 import LoginForm from "./Components/Auth/LoginForm";
+import MembersForm from "./Components/Members/MembersForm";
+import NewsForm from "./Components/News/NewsForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import UserForm from "./Components/Users/UsersForm";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact />
-          <Route path="/activities/:id" component={ActivitiesDetail} />
+          <Route path="/" exact component={} />
+          <Route path="/activities/:id" />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/backoffice/home" component={HomeForm} />
-          <Route
-            path="/backoffice/members/edit/:id"
-            component={MembersCreateEdit}
-          />
-          <Route
-            path="/backoffice/members/edit"
-            component={MembersCreateEdit}
-          />
+          <Route path="/backoffice/home"/>
+          <Route path="/backoffice/members/edit/:id"/>
+          <Route path="/backoffice/members/edit" />
           <Route path="/backoffice/news/:id" component={NewsForm} />
           <Route path="/backoffice/news/" component={NewsForm} />
-          <Route path="/backoffice/organization" component={Organization} />
-          <Route
-            path="/backoffice/organization/edit"
-            component={UpdateDataForm}
-          />
+          <Route path="/backoffice/organization" />
+          <Route path="/backoffice/organization/edit"/>
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-news" component={NewsForm} />
-          <Route path="/backoffice" component={ScreenDashboard} />
+          <Route path="/backoffice" />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/login" component={LoginForm} />
-          <Route path="/news" component={NewsDisplay} />
-          <Route path="/novedades/:id" component={NewsDetail} />
+          <Route path="/news" />
+          <Route path="/novedades/:id" />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
         </Switch>
