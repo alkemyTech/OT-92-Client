8a04b456456c599501d646b88dd86ad78eee74d3
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-04 20:04:05 -0300

    Added the ONG logo and some styles.
    Added the logo at the center top of the page, above the main title, as well as a description. Also implemented some styles at "index.css".

--- src/Components/Contact/ContactData.js
+++ src/Components/Contact/ContactData.js
@@ -8,7 +8,6 @@ export default function datasList() {
             linkedin_url: "www.linkedin.com/company/somosmas",
             instagram_url: "www.instagram.com/SomosMás",
             twitter_url: "www.twitter.com/somosmas",
-            logo: "http://ongapi.alkemy.org/storage/4ZR8wsUwr9.png"
         }
     ]
 
@@ -20,12 +19,20 @@ export default function datasList() {
                     <div className="container">
                         <div>
                             <div className="card-body">
-                                <img src={data.logo} />
-                                <h5>Celular: {data.cellphone}</h5>
-                                <li><a href="www.facebook.com/Somos_Más">{data.facebook_url}</a></li>
-                                <li><a href="www.linkedin.com/company/somosmas" >{data.linkedin_url}</a></li>
-                                <li><a href="www.instagram.com/SomosMás" >{data.instagram_url}</a></li>
-                                <li><a href="www.twitter.com/somosmas" >{data.twitter_url}</a></li>
+                                <p>Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y 
+                                papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de 
+                                inserción social. Uniendo las manos de todas las familias, las que viven en el barrio
+                                y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos.
+                                ﻿ Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de
+                                dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la
+                                comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro
+                                comunitario que acompaña a más de 700 personas a través de las áreas de: Educación,
+                                deportes, primera infancia, salud, alimentación y trabajo social.</p>
+                                <h6>Celular: {data.cellphone}</h6>
+                                <h6>Facebook: <a href="www.facebook.com/Somos_Más">{data.facebook_url}</a></h6>
+                                <h6>LinkedIn: <a href="www.linkedin.com/company/somosmas">{data.linkedin_url}</a></h6>
+                                <h6>Instagram: <a href="www.instagram.com/SomosMás" >{data.instagram_url}</a></h6>
+                                <h6>Twitter: <a href="www.twitter.com/somosmas" >{data.twitter_url}</a></h6>
                             </div>
                        </div>
                    </div>    
--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ContactData from './ContactData'
 
+import './index.css'
+
 
 
 export default function Contact() {
     return (
         <div>
+            <img src={"http://ongapi.alkemy.org/storage/4ZR8wsUwr9.png"} />
             <h1>Contacto</h1>
             <ContactData />
         </div>
