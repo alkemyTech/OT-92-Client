d5cf0b781050380c84c5d866c3193202094670df
fdelavalle <delavallef4@gmail.com>
2021-11-04 12:38:36 -0300

    linked services to activities components

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -1,22 +1,25 @@
-import { useParams } from "react-router-dom";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import './ActivitiesDetail.css';
+import { activitiesService } from '../../../Services/privateApiService';
 const ActivitiesDetail = () => {
   let { id: paramsId } = useParams();
+
   const activities = [
     {
-      id: "1",
-      title: "Recorrido por el comedor",
-      body: "se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna.",
+      id: '1',
+      title: 'Recorrido por el comedor',
+      body: 'se recorre el comedor del area central, se le pregunta a la gente que es lo que mas le gusta comer y comimos de postre unos alfajores donados por Havanna.',
     },
     {
-      id: "2",
-      title: "Visitas clases de ingles",
-      body: "se visita a los alumnos de primaria en sus clases de ingles. ",
+      id: '2',
+      title: 'Visitas clases de ingles',
+      body: 'se visita a los alumnos de primaria en sus clases de ingles. ',
     },
     {
-      id: "3",
-      title: "torneo de futbol 5 ",
-      body: "los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo.",
+      id: '3',
+      title: 'torneo de futbol 5 ',
+      body: 'los chicos que cursan el nivel secundario organizaron un torneo de futbol 5 el dia domingo.',
     },
   ];
   const paramsActivity = activities.filter(
@@ -26,13 +29,19 @@ const ActivitiesDetail = () => {
     <>
       <div>
         {paramsActivity[0] ? (
-          <div className="container d-flex flex-column justify-items-center mt-5">
-            <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
-            <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1>
+          <div className='container d-flex flex-column justify-items-center mt-5'>
+            <h1 className=' h1 w-100 text-center Activities-title'>
+              {paramsActivity[0].title}
+            </h1>
+            <h1 className='h3 Activities-body text-justify'>
+              {paramsActivity[0].body}
+            </h1>
           </div>
         ) : (
-          <div className="mt-5 container">
-            <h1 className="Activities-title ">Oh Oh! hemos tenido problemas con esa actividad :( </h1>
+          <div className='mt-5 container'>
+            <h1 className='Activities-title '>
+              Oh Oh! hemos tenido problemas con esa actividad :({' '}
+            </h1>
           </div>
         )}
       </div>
--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,66 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+  headers: {
+    Group: 92,
+  },
+};
+
+// service for activities
+export const activitiesService = {
+  //fetch all activities
+  getActivities: async () => {
+    const data = await axios.get('http://ongapi.alkemy.org/api/activities');
+    try {
+      return data;
+    } catch (error) {
+      console.log(error);
     }
-}
-
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-
-export default Get
+  },
+  //fetch one activity
+  getActivity: async (id) => {
+    const data = await axios.get(
+      `http://ongapi.alkemy.org/api/activities/${id}`
+    );
+    try {
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //create activity
+  createActivity: async (activity) => {
+    const data = await axios.post(
+      `http://ongapi.alkemy.org/api/activities/create`,
+      activity
+    );
+    try {
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //update activity
+  updateActivity: async (activity) => {
+    const data = await axios.put(
+      `http://ongapi.alkemy.org/api/activities/${activity.id}`,
+      activity
+    );
+    try {
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //delete activity
+  deleteActivity: async (id) => {
+    const data = await axios.delete(
+      `http://ongapi.alkemy.org/api/activities/${id}`
+    );
+    try {
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
