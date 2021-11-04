86b9e5197ad36a4ff97a098a2d5db7bea4d6db5a
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-03 21:30:21 -0300

    Header correction and deleteById function

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 92,
+        Authorization: null,
     }
 }
 
@@ -13,3 +14,18 @@ const Get = () => {
 }
 
 export default Get
+
+const queryDeleteById = async (section, id) => {
+    const url = `http://ongapi.alkemy.org/api/${section}/${id}`;
+    const configuration = getAuthorization();
+    /* Este metodo getAuthorization() fue desarrollado en el ticket 69
+    por Facundo Delavalle */
+    let res = await axios.delete(url, configuration);
+    try {
+        console.log(`Deleted succesfully`);
+        console.log(res.status);
+    } catch (error) {
+        console.log("Something went wrong: " + error)
+    }
+}
+export { queryDeleteById };
