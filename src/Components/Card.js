e658fc74d122ddb6d716563b3d9c3349d75eefc0
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-11-04 19:39:49 -0300

    basic card structure done

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-const Card = (props) => {
-    return (
-        <div>
-            <h1>card</h1>
-        </div>
-    )
-}
+const Card = props => {
+  const imgPlaceholder = 'https://via.placeholder.com/150.png?text=Sin+imagen';
 
-export default Card
+  return (
+    <div className="card m-3" style={{ width: '18rem' }}>
+      <img
+        src={props.img || imgPlaceholder}
+        className="card-img-top"
+        alt={props.title}
+      />
+      <div className="card-body d-flex flex-column align-items-center">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
