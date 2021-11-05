81ab9a732ee8f806362f3182547fd4e9b660fbb5
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-05 17:50:24 -0300

    App.js duplicate imports and routes fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './App.css'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -21,28 +20,6 @@ import ActivitiesDetail from './Components/Activities/Detail/ActivitiesDetail';
 import HomeForm from './Components/Home/HomeForm';
 import NewsDisplay from './Components/News/NewsDisplay';
 import LoginForm from './Components/Auth/LoginForm';
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ActivitiesForm from "./Components/Activities/ActivitiesForm";
-import CategoriesForm from "./Components/Categories/CategoriesForm";
-import NewsForm from "./Components/News/NewsForm";
-import SlidesForm from "./Components/Slides/SlidesForm";
-import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
-import UserForm from "./Components/Users/UsersForm";
-import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
-import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
-import ProjectsForm from "./Components/Projects/ProjectsForm";
-import ScreenDashboard from "./Components/Backoffice/ScreenDashboard";
-import NewsDetail from "./Components/News/Detail";
-import Organization from "./Components/Organization/Organization";
-import MembersCreateEdit from "./Components/Members/MembersCreateEdit";
-import UpdateDataForm from "./Components/Organization/UdpateDataForm";
-import ActivitiesDetail from "./Components/Activities/Detail/ActivitiesDetail";
-import HomeForm from "./Components/Home/HomeForm";
-import NewsDisplay from "./Components/News/NewsDisplay";
-import LoginForm from "./Components/Auth/LoginForm";
 
 
 function App() {
@@ -52,9 +29,6 @@ function App() {
         <Switch>
 
           <Route path="/" exact  />
-
-          <Route path="/" exact />
-
           <Route path="/activities/:id" component={ActivitiesDetail} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/backoffice/home" component={HomeForm} />
@@ -78,7 +52,6 @@ function App() {
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice" component={ScreenDashboard} />
-          <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
