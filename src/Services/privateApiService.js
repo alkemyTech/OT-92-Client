9561bb88b193968cb37bbc45241f24fb615c3cc4
tankez0r <martintorres1992@gmail.com>
2021-11-05 00:24:26 -0300

    Service testeado, funciona correcto y puede ser reutilizados por todos los componentes que lo necesitan, teniendo como datos obligatorios un String que diga cual es la seccion que lo necesita y un queryObject que es un objeto donde estan los datos que se intentan enviar a la API

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 92             //Aqui va el ID del equipo!!
+        Group: 92                //Aqui va el ID del equipo!!
     }
 }
 
-const queryPutData = (queryObject) => {
-    const url = "http://ongapi.alkemy.org/api/"
-    const axiosPut = axios.put(url, queryObject, config)
+const queryPutData = async (section, queryObject) => {
+    const url = `http://ongapi.alkemy.org/api/${section}/${queryObject.id}`
+    const axiosPut = await axios.put(url, queryObject, config)
+    console.log("asdss")
     try {
         console.log(axiosPut.data)
     } catch (error) {
