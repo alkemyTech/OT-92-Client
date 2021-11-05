d2bad75f22cac03d28e58978152fbf782a5b60c2
tankez0r <martintorres1992@gmail.com>
2021-11-05 17:50:44 -0300

    Rehaciendo el form encuentro un problema, y estoy mas cerca de una solucion. Necesito ayuda. En la evaluación de CKEditor intento que se renderice de forma asincronica y espere la respuesta para evaluar si existe o no una actividad a editar. Cuando le doy solo un parametro funciona bien, pero al agregar un else, la segunda parte ya no se evalua. Intento hacer una evaluacion desde el get de la actividad.

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import '../FormStyles.css';
 import EditorField from "./EditorField"
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form, ErrorMessage, Field } from 'formik';
 import * as Yup from "yup";
 import axios from 'axios';
-import queryPutData from '../../Services/privateApiService';
 
 const ActivitiesForm = () => {
     const [formValues, setValues] = useState({ name: "", image: null, description: "", complete: false, id: undefined });
     const [isBase64, setIsBase64] = useState(false)
-    const [triggerCreate, setCTrigger] = useState(false)
-    const [activityIsReady, setActivityReady] = useState(false)
-    const [editTrigger, setEditTrigger] = useState(false)
     const [imagenPreview, setImagenPreview] = useState(null)
     const [initialValues, setInitialValues] = useState({ name: "", image: null, description: "", complete: false, id: undefined })
+    const [descriptionValue, setDescriptionValue] = useState("")
+    const [formIsComplete, isComplete] = useState(false);
+    const [TriggerEditar, setTriggerEditar] = useState(false)
+    const [activityIsReady, setactivityIsReady] = useState(false)
+    const [renderNewCK, setrenderNewCK] = useState(false)
+
 
 
     /* en este useEffect se van a filtrar las actividades, ya sea para
@@ -23,82 +25,58 @@ const ActivitiesForm = () => {
     useEffect(() => {
         // parse file to base64 and return it to formState
         const setbase64 = (file) => {
-            const metaData = `data:${formValues.image.type}; base64, `
-            const reader = new FileReader();
-            reader.readAsBinaryString(file)
-            reader.onload = () => { setValues({ ...formValues, image: metaData + btoa(reader.result) }); setImagenPreview(metaData + btoa(reader.result)) }    //
-            setIsBase64(true)
-
-
+            console.log(typeof file)
+            // const metaData = `data:${formValues.image.type}; base64, `
+            // const reader = new FileReader();
+            // reader.readAsBinaryString(file)
+            // reader.onload = () => { setValues({ ...formValues, image: metaData + btoa(reader.result) }); setImagenPreview(metaData + btoa(reader.result)) }    //
+            // setIsBase64(true)
         }
-
-        // condiciones para transformar la imagen y setearlas en el formulario
         if (formValues.image != null && isBase64 === false && formValues.id === undefined) {
             setbase64(formValues.image)
-        } else if (formValues.image === null) {
-            setIsBase64(false)
         } else if (formValues.image != null && isBase64 === false && formValues.id !== undefined) {
             const imageUrl = formValues.image;
-            const xhr = new XMLHttpRequest();
-            xhr.open("GET", imageUrl, true);
-            xhr.responseType = "blob"
-            xhr.send();
-            xhr.onload = () => { setbase64(xhr.response) }
-        }
-
-        // create activity
-        if (triggerCreate) {
-            const createActivity = async () => {
-                const queryObject = { name: formValues.name, description: formValues.description, image: formValues.image }
-                const url = "http://ongapi.alkemy.org/api/activities"
-                const data = await axios.post(url, queryObject)
-                try {
-                    console.log(data)
-                    console.log(queryObject)
-                    setCTrigger(false)
-                }
-                catch (error) {
-                    console.log(error)
-                    setCTrigger(false)
-                }
-            }
-            createActivity();
-        }
-
-        //edit activity
-        if (editTrigger === true) {
-            const queryObject = {
-                name: formValues.name, description: formValues.description, image: formValues.image, id: formValues.id
-            }
-            queryPutData("activities", queryObject)
-            console.log("se puede editar(?")
-
+            setbase64(imageUrl)
         }
 
 
-        // get activity by id
+
+        /// get activity
+
+
+
         const paramString = new URLSearchParams(window.location.search);
         const activityID = paramString.get("id")
-        if (activityIsReady === false && activityID != null) {
+        if (activityID != null && activityIsReady === false) {
             const getActivity = async () => {
                 const url = "http://ongapi.alkemy.org/api/activities/" + activityID;
                 let res = await axios.get(url);
                 try {
                     let data = res.data;
                     setValues(data.data)
-                    setActivityReady(true)
+                    setactivityIsReady(true)
                     setImagenPreview(formValues.image)
+                    setrenderNewCK(true)
                     setInitialValues(data.data)
                 } catch (error) {
                     console.log(error)
                 }
             }
             getActivity()
+            setDescriptionValue(formValues.description)
         }
+        setrenderNewCK(true)
+        setDescriptionValue(formValues.description)
 
 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [isBase64, formValues, triggerCreate, editTrigger])
+
+
+
+    }
+
+
+
+        , [isBase64, formValues])
 
 
     // dummy preview
@@ -119,19 +97,29 @@ const ActivitiesForm = () => {
     // funcion devolver botones 
 
     const Botonera = () => {
-        if (formValues.complete === true) {
+        if (formIsComplete) {
             if (formValues.id === undefined) {
-                return (<> <button className="submit-btn" onClick={() => { setCTrigger(true) }}>Crear</button>
+                return (<> <button className="submit-btn" onClick={() => { }}>Crear</button>
                     <button className="submit-btn" onClick={() => { setValues(initialValues); setImagenPreview(null) }}>Deshacer Cambios</button>  </>)
             }
             else if (formValues.id !== undefined) {
                 return (<>
-                    <button className="submit-btn" onClick={() => { setEditTrigger(true) }}>Editar</button>
-                    <button className="submit-btn" onClick={() => { setValues(initialValues); setImagenPreview(null); setActivityReady(false) }}>Deshacer Cambios</button>
+                    <button className="submit-btn" onClick={() => { setTriggerEditar(true) }}>Editar</button>
+                    <button className="submit-btn" onClick={() => { setValues({ name: "", image: null, description: "", complete: false, id: undefined }); setImagenPreview(null) }}>Deshacer Cambios</button>
                 </>)
             }
         }
 
+
+    }
+
+    // funcion CKEditor 
+    const CKEditorLoader = (formik) => {
+
+        if (renderNewCK && formValues.id != undefined) {
+            return (<EditorField formik={formik} initialValue={descriptionValue} />
+            )
+        }
 
     }
 
@@ -155,7 +143,7 @@ const ActivitiesForm = () => {
             <Formik initialValues={initialValues}
                 validationSchema={validate}
                 enableReinitialize={true}
-                onSubmit={(values) => { setValues({ ...values, complete: true }); setImagenPreview(URL.createObjectURL(values.image)) }}
+                onSubmit={(values) => { isComplete(true); setImagenPreview(URL.createObjectURL(values.image)) }}
             >
                 {formik => (
                     <Form className="form-container" >
@@ -172,8 +160,7 @@ const ActivitiesForm = () => {
                         <ErrorMessage name="image"
                             render={(msg) => <span className="error"> {msg} </span>}
                         />
-
-                        <EditorField formik={formik} />
+                        {CKEditorLoader(formik)}
 
                         <button className="submit-btn" type="submit" >Send</button>
                     </Form>
