a31265d89e222771689b7a41a6b3b6b860b71939
fdelavalle <delavallef4@gmail.com>
2021-11-05 10:20:57 -0300

    added link to create a new

--- src/Components/Backoffice/NewsListBackOffice.js
+++ src/Components/Backoffice/NewsListBackOffice.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import '../FormStyles.css';
 
 const NewsListBackOffice = () => {
+  //mock data
   const newsMock = [
     {
       id: 2,
@@ -32,26 +33,36 @@ const NewsListBackOffice = () => {
 
   return (
     <>
-      <h1 className='mt-5'>Lista de noticias</h1>
+      <div className='d-flex align-items-center flex-column justify-content-around flex-lg-row'>
+        <h1 className='mt-5'>Listado de novedades</h1>
+
+        <div className='mt-5 m-auto'>
+          <Link to='/backoffice/news/create' className='btn btn-success'>
+            Crear noticia
+          </Link>
+        </div>
+      </div>
       <div className='mt-5 container'>
         {newsMock.map((news) => (
           <div
             key={news.id}
-            className='d-flex rounded bg-grey h-10 m-3 p-2 align-items-center justify-content-between'
+            className='d-flex rounded bg-grey h-10 m-3 p-2 align-items-center justify-content-between flex-column flex-lg-row'
           >
             <div className='d-flex align-items-center px-2'>
-              <img
-                src={news.image}
-                className='rounded'
-                alt={news.name}
-                style={{ width: '5rem' }}
-              />
-              <h4 className='ml-3'>{news.name}</h4>
+              <div className='d-flex align-items-center'>
+                <img
+                  src={news.image}
+                  className='rounded'
+                  alt={news.name}
+                  style={{ width: '5rem' }}
+                />
+                <h4 className='ml-3'>{news.name}</h4>
 
-              <p className='ml-2 mt-2'>{news.createdAt}</p>
+                <p className='ml-3 mt-2'>{news.createdAt}</p>
+              </div>
             </div>
             <div className='p-1'>
-              <button className='btn btn-warning mr-3'>Editar</button>
+              <button className='btn btn-secondary mr-3'>Editar</button>
               <button className='btn btn-danger'>Eliminar</button>
             </div>
           </div>
