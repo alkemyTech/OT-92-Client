cccce0c4262d68b9337d92ddf9496e732151fb88
Gabriel <ivangabrielalabi@gmail.com>
2021-11-05 11:38:50 -0300

    make the function GetWithID on PublicApiService

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,14 +2,19 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 92,                //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
+export const GetWithId = async (link, id) => {
+ const pageId = id ? id : '';
+    await axios.get(`${link}/${pageId}`).then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
+
+export const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
-
-export default Get
