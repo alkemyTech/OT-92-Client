e18c3cce49ff5832463bfbb87300b5c32c37c250
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-07 03:07:10 -0300

    Component creation, form-container and title

---
+++ src/Components/Contact/ContactForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactForm = () => {
+    return (
+        <div className="form-container">
+            <h3>Contactanos</h3>
+            
+        </div>
+    )
+}
+
+export default ContactForm
