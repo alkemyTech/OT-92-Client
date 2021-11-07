574e54d2015117d6c42ddccfe802d9a0e045f819
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-07 03:08:30 -0300

    Formik, Yup and TextInput component imports

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import TextInput from '../Newsletter/TextInput';
 
 const ContactForm = () => {
     return (
         <div className="form-container">
             <h3>Contactanos</h3>
-            
+
         </div>
     )
 }
