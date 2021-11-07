097021e93b885a15abfd4289f5ea585c2fee740b
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-07 11:46:13 -0300

    Implemented the "Title.js" component
    Imported the generic title component into "/Contact/index.js" and used it there to display the page's main title.

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ContactData from './ContactData'
-import GenericTitle from './GenericTitle'
+import Title from '../Title'
 
 import './index.css'
-
-
 
 export default function Contact() {
     return (
         <div>
-            <GenericTitle />
+            <Title content={{title: 'Contacto'}} />
+            <br/>
             <img src={"http://ongapi.alkemy.org/storage/4ZR8wsUwr9.png"} />
+            <br/>
             <ContactData />
         </div>
     )
