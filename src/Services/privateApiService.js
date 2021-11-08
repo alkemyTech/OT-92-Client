99a98c4460d3850785b72a00bd31c698f48ca6ca
gwynlordofcinder <pabloandrestejedor@icloud.com>
2021-11-07 21:04:59 -0300

    privateGet method added

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const config = {
   headers: {
     Group: 92,
@@ -7,9 +8,19 @@ const config = {
 
 //function that gets the token from local storage and returns a headers with authorization object
 export const getAuthorization = () => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+};
+
+export const privateGet = async (url, id) => {
+  try {
+    const endPointId = id ? `/${id}` : '';
+    const resp = await axios.get(url + endPointId, getAuthorization());
+    return resp;
+  } catch (error) {
+    console.log(error);
+  }
 };
