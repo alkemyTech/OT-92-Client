ce9fd9d9f99044d018f43782617a9ee26ce21d35
Nico Vazquez <vazquez.n.g@gmail.com>
2021-11-08 01:05:08 -0300

    Update pr OT92-59-crear-componente-para-renderizar

--- src/Components/Donations/Donations.js
+++ src/Components/Donations/Donations.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './Donations.css';
-import {useHistory} from 'react-router-dom';
 const Donations = () => {
-    const history = useHistory();
     const donate = 'Para nosotros es muy importante la ayuda que nos puedas dar, nos permite seguir creciendo y haciendo que cada vez podamos hacer mas por la educación y la nutrición de los chicos que lo necesitan. Te dejamos aca un link para que colabores con lo que puedas: ';
     return ( 
         <>
         <div className="container d-flex flex-column align-items-center">
             <h1 className="mt-5 Donations-title"> {donate} </h1>
-            <a href="https://mpago.la/1CCtqMi"  target="_blank"> <button type="button" class="btn btn-primary ">Pagar</button> </a>
-        </div>
+            <div className="d-flex justify-content-around w-100 mt-4 flex-wrap">
+            <a href="https://mpago.la/1CCtqMi"  target="_blank" rel="noreferrer"> <button type="button" class="btn btn-primary p-3 button-text btn-lg ">$ 500</button> </a>
+            <a href="https://mpago.la/1haGpy4"  target="_blank" rel="noreferrer"> <button type="button" class="btn btn-primary p-3 button-text btn-lg">$1000</button> </a>
+            <a href="https://mpago.la/1NB9QoG"  target="_blank" rel="noreferrer"> <button type="button" class="btn btn-primary p-3 button-text btn-lg">$5000</button> </a>
+            <a href="https://mpago.la/1UVV6X6"  target="_blank" rel="noreferrer"> <button type="button" class="btn btn-primary p-3 button-text btn-lg">$10000</button> </a>
+            </div>
+            </div>
         </>
      );
 }
- 
+
 export default Donations;
