338ec3c72ab38ab20a6ca0d2c3f158376a66eebb
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-08 23:25:02 -0300

    Card item using bootstrap

--- src/Components/News/NewsItem.js
+++ src/Components/News/NewsItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const NewsItem = () => {
+const NewsItem = ({ content }) => {
+
     return (
-        <div>
-            
+        <div className="card" style={{width: "18rem"}}>
+            <img 
+                src={content.image} 
+                className="card-img-top" 
+                alt={content.title} 
+                onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "/error image somos mas.jpg";
+                }}
+            />
+            <div className="card-body">
+                <h5 className="card-title">{content.title}</h5>
+                <Link to={`/novedades/${content.id}`}>
+                    <button type="button" className="btn btn-primary">
+                        Ver más
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
