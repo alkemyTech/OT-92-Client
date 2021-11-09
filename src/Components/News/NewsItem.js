bbdb90fc188f40f20b95244a3044b7890e688b4d
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-08 23:59:56 -0300

    Button replaced with cardLink and changes in the Link style

--- src/Components/News/NewsItem.js
+++ src/Components/News/NewsItem.js
@@ -4,25 +4,22 @@ import { Link } from 'react-router-dom'
 const NewsItem = ({ content }) => {
 
     return (
-        <div className="card mx-auto my-3 customize-card" style={{width: "18rem"}}>
-            <img 
-                src={content.image} 
-                className="card-img-top" 
-                alt={content.title} 
-                onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = "/error image somos mas.jpg";
-                }}
-            />
-            <div className="card-body">
-                <h5 className="card-title">{content.title}</h5>
-                <Link to={`/novedades/${content.id}`}>
-                    <button type="button" className="btn btn-dark float-right">
-                        Ver más
-                    </button>
-                </Link>
+        <Link to={`/novedades/${content.id}`} className="link">
+            <div className="card mx-auto my-3 customize-card" style={{width: "18rem"}}>
+                <img 
+                    src={content.image} 
+                    className="card-img-top" 
+                    alt={content.title} 
+                    onError={(e) => {
+                        e.target.onerror = null;
+                        e.target.src = "/error image somos mas.jpg";
+                    }}
+                />
+                <div className="card-body">
+                    <h5 className="card-title">{content.title}</h5>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
