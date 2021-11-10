15c8672455f487bdec57a4111118f4bf8eec7ee5
Edwin Alexis <edbwinn@gmail.com>
2021-11-10 17:01:09 -0300

    progress or wait component creation
    create the component with the waiting spinner in the assets / SpinnerComponent folder

--- src/App.js
+++ src/App.js
@@ -36,9 +36,7 @@ function App() {
     <>
       <BrowserRouter>
         <Switch>
-
           <Route path="/" exact component={Home} />
-
           <Route path="/activities/:id" component={ActivitiesDetail} />
           <Route path="/backoffice" exact component={ScreenDashboard} />
           <Route exact path="/backoffice/activities" component={ActivitesList}/>
@@ -47,27 +45,14 @@ function App() {
           <Route path="/backoffice/home" component={HomeForm} />
           <Route path="/backoffice/slides" component={SlidesListBo} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route
-            path='/backoffice/categories'
-            component={CategoriesListBackoffice}
-          />
-          <Route
-            path='/backoffice/members/edit/:id'
-            component={MembersCreateEdit}
-          />
-          <Route
-            path='/backoffice/members/edit'
-            component={MembersCreateEdit}
-          />
+          <Route path='/backoffice/categories'component={CategoriesListBackoffice}/>
+          <Route path='/backoffice/members/edit/:id'component={MembersCreateEdit}/>
+          <Route path='/backoffice/members/edit'component={MembersCreateEdit}/>
           <Route path='/backoffice/news/:id' component={NewsForm} />
           <Route path='/backoffice/news/' component={NewsListBackOffice} />
           <Route path='/backoffice/organization' component={Organization} />
-          <Route
-            path='/backoffice/organization/edit'
-            component={UpdateDataForm}
-          />
+          <Route path='/backoffice/organization/edit'component={UpdateDataForm}/>
           <Route path='/backoffice/users' component={UsersListContainer} />
-
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path='/create-category' component={CategoriesForm} />
@@ -75,16 +60,11 @@ function App() {
           <Route path='/create-news' component={NewsForm} />
           <Route path='/create-testimonials' component={TestimonialForm} />
           <Route path='/create-user' component={UserForm} />
-          
           <Route path="/donaciones" component={Donations} />
-
-
           <Route path="/login" component={LoginForm} />
-        
           <Route path="/news" component={NewsDisplay} />
           <Route path="/novedades/:id" component={NewsDetail} />
           <Route path="/nosotros" component={About} />
-
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/activities" component={ActivitiesDisplay} />
