40c9d952850a355e8b5c9fcf7fa8d6e9f9060e57
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-10 15:52:35 -0300

    Updated categories

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,32 +1,93 @@
 import React, { useState } from 'react';
 import '../FormStyles.css';
 
+import axios from 'axios';
+import EditorField from "../Activities/EditorField";
+import { Formik, Form, ErrorMessage } from 'formik';
+import {useParams} from 'react-router'
+import * as Yup from "yup";
+
+
 const CategoriesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    })
+  const {id} = useParams
+  const [initialValues, setInitialValues] = useState({
+    name: '',
+    id: "",  
+    image: "",
+    description: ""
+  });
+  
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      
+      if(!id) {
+          axios.post('http://ongapi.alkemy.org/api/categories')
+      }
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
+  // validaciones de YUP para el form
+  const validate = Yup.object({
+    name: Yup.string()
+      .required("Se requiere un nombre")
+      .min(4, "Se requieren al menos cuatro caracteres"),
+    description: Yup.string()
+      .required("Este campo es obligatorio"),
+    image: Yup.mixed().required('Es necesario subir una foto'),
+  });
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
+  return (
+
+    <div className="container">
+
+      <Formik initialValues={initialValues}
+        validationSchema={validate}
+        enableReinitialize={true}
+        onSubmit={(values) => { setInitialValues({ ...values }) }}
+      >
+        {formik => (
+          <Form className="form-container" onSubmit={handleSubmit}>
+            <input className="input-field" autoComplete="off"
+              type="text" name="name" onChange={formik.handleChange} value={formik.values.name}
+              onBlur={formik.handleBlur}
+              placeholder="Nombre"></input>
+            <ErrorMessage name="name"
+              render={(msg) => <span className="error"> {msg} </span>}
+            />
+
+            <EditorField formik={formik} initialValue={initialValues.description} />
+
+            <input 
+            type="file" 
+            name="image" 
+            accept="image/png, image/jpeg" 
+            onChange= {event => {
+                let file = event.currentTarget.files[0];
+                let reader = new FileReader();
+                reader.readAsBinaryString(file);
+
+                reader.onload = function () {
+                  let codedImg = window.btoa(reader.result);
+                  setInitialValues({
+                    ...initialValues,
+                    profilePhoto: codedImg,
+                  });
+                };
+                reader.onerror = function () {
+                  console.log('Falló la carga de la imagen');
+                };
+              }}
+            onBlur={formik.handleBlur} />
+            <ErrorMessage name="image"
+              render={(msg) => <span className="error"> {msg} </span>}
+            />
+
+            <button className="submit-btn" type="submit" >Enviar</button>
+          </Form>
+        )}
+      </Formik>
+    </div >
+
+  );
 }
- 
 export default CategoriesForm;
