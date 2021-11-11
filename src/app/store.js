48f7687abba642d7b8adf1bd8b4413731c2e9af6
fdelavalle <delavallef4@gmail.com>
2021-11-11 11:58:26 -0300

    created activitiesSlice and implemented api call

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import activitiesReducer from '../../src/store/activity/activitySlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    activities: activitiesReducer,
   },
 });
