8db7216368d280a550a5974875cc0ee6192b13a4
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-12 15:01:11 -0300

    Imported the reducer.
    Imported and added "categoriesReducer".

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import categoriesReducer from './category/categorySlice'
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    categories: categoriesReducer
   },
 });
