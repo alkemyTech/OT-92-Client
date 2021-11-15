2270cc315604e51bc78342f8d819b81ae3387ad3
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-15 13:34:03 -0300

    Updated "index.js".
    Made some changes to make it more amiable.

--- src/Components/NotFound/index.js
+++ src/Components/NotFound/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <Link to="/">Go Home</Link>
-  </div>
+ <div class="page-wrap d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <span class="display-1 d-block">404</span>
+                <div class="display-5 d-block">¡Oopsss!</div>
+                <div class="mb-4 lead">La página que buscas no existe.</div>
+                <a href="https://www.totoprayogo.com/#" class="btn btn-link">Volver al Inicio</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 );
 
 export default NotFound;
