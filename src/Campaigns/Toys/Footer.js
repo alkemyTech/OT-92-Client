27ef625c728d71e6e034f7e8c507f25a556c7c33
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2021-11-18 14:25:52 -0300

    Building the footer.
    Built a footer and added the ONG's logo and social media profiles to it.

--- src/Campaigns/Toys/Footer.js
+++ src/Campaigns/Toys/Footer.js
@@ -1,10 +1,74 @@
 import React from 'react';
 
+import {TiSocialFacebook} from 'react-icons/ti'
+import {TiSocialInstagram} from 'react-icons/ti'
+import {TiSocialTwitter} from 'react-icons/ti'
+import {TiSocialLinkedin} from 'react-icons/ti'
+
 const Footer = () => {
   return (
-    <div>
+    <div >
+   
+
+
+    
+    <footer className="bg-light text-center text-white">
+    <img src={"http://ongapi.alkemy.org/storage/4ZR8wsUwr9.png"} alt=""/>
+    <hr class="bg-dark border-1 border-top border-dark" />
+    <div className="container p-4 pb-0">
+  
+      <section className="mb-4">
+        
+        <a
+          className="btn "
+          style={{backgroundcolor: "#3b5998"}}
+          href="#!"
+          role="button"
+          ><TiSocialFacebook />
+          </a>
+  
+        
+        <a
+          className="btn "
+          style={{backgroundcolor: "#55acee;"}}
+          href="#!"
+          role="button"
+          ><TiSocialTwitter />
+          </a>
+  
+      
+        <a
+          className="btn "
+          style={{backgroundcolor: "#ac2bac;"}}
+          href="#!"
+          role="button"
+          ><TiSocialInstagram />
+          </a>
+  
+        
+        <a
+          className="btn rounded-circle"
+          style={{backgroundcolor: "black"}}
+          href="#!"
+          role="button"
+          ><TiSocialLinkedin />
+          </a>
+        
+      </section>
       
     </div>
+    
+  
+  
+    <div className="text-center p-3 text-black" >
+      © 2021 by Alkemy {" "}
+      <a className="text-black" href="https://alkemy.org">All rights reserved.</a>
+    </div>
+    
+  </footer>
+    
+  </div>
+  
   );
 }
  
