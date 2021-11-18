35736e32643b6590b3f96c603caf2c418d6eb46f
Edwin Alexis <edbwinn@gmail.com>
2021-11-18 17:32:52 -0300

    renaming the Slices folder to backOffice in the store
    renaming the Slices folder to backOffice in the store

--- src/Components/Backoffice/Slides/SlidesListBO.jsx
+++ src/Components/Backoffice/Slides/SlidesListBO.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getSlicesApi } from '../../../store/Slices/SlicesBackOffice';
+import { getSlicesApi } from '../../../store/backOffice/SlicesBackOffice';
 import SlideComponent from './SlideComponent';
 
 const SlideListBackOffice = () => {
