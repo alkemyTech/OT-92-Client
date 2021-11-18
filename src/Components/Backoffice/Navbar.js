198b136f5b1ba6da9289f586160440547dba1c60
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-18 01:45:24 -0300

    Sidebar button

--- src/Components/Backoffice/Navbar.js
+++ src/Components/Backoffice/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import "./Navbar.css";
+import { Button } from 'react-bootstrap'
+import { MdMenu } from 'react-icons/md'
 
 const Navbar = () => {
     const [show, setShow] = useState(false);
@@ -7,10 +9,12 @@ const Navbar = () => {
     const handleShow = () => setShow(!show);
 
     return (
-        <div>
-            
-        </div>
-    )
+        <>
+        <Button variant="primary" onClick={handleShow}>
+            <MdMenu />
+        </Button>
+        </>
+    );
 }
 
 export default Navbar
