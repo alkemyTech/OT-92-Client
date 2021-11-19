63a914ba6c293b6e1bd7d680b1b094ec1ce1d1f6
fdelavalle <delavallef4@gmail.com>
2021-11-18 21:16:43 -0300

    added skeleton and alerts to the news display

--- src/Components/News/NewsDisplay.js
+++ src/Components/News/NewsDisplay.js
@@ -3,27 +3,50 @@ import Title from '../Title/Title';
 import './Detail/NewsFormat.css';
 import NewsList from './NewsList';
 import { getNews } from '../../Services/publicApiService';
+import Skeleton from '../Skeleton/Skeleton';
+import { errorAlert } from '../../Assets/Alerts/alerts';
 
 const NewsDisplay = () => {
   const [news, setNews] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       const res = await getNews();
-      setNews(res.data.data);
+      if (res.status === 200) {
+        setNews(res.data.data);
+      } else {
+        setError(true);
+      }
     };
     fetchData();
   }, []);
 
   return (
     <>
+      {error
+        ? errorAlert({
+            title: 'Hubo un problema',
+            text: 'No se pudieron recuperar las novedades!',
+            time: '2000',
+          })
+        : null}
       <div className='my-3' />
-      <Title
-        content={{
-          title: 'Novedades',
-          image: null,
-        }}
-      />
+      {news.length === 0 ? (
+        <div className='d-flex justify-content-center'>
+          <Skeleton type='title' width='550' height='75' />
+        </div>
+      ) : (
+        <>
+          <Title
+            content={{
+              title: 'Novedades',
+              image: null,
+            }}
+          />
+        </>
+      )}
+
       <div className='container'>
         <NewsList newsData={news} />
       </div>
--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import '../CardListStyles.css';
-import NewsItem from "./NewsItem";
+import NewsItem from './NewsItem';
+import Skeleton from '../Skeleton/Skeleton';
 
 const NewsList = ({ newsData }) => {
-
-    return (
+  return (
+    <>
+      {newsData.length > 0 ? (
         <>
-            {newsData.length > 0 ? (
-                <>
-                    <div className="row my-3">
-                        {newsData.map((article) => (
-                            <div className="col-sm-12 col-md-6 col-lg-4 justify-content-center">
-                                <NewsItem
-                                    key={article.id}
-                                    content={article}
-                                />
-                            </div>
-                        ))}
-                    </div>
-                </>
-            ) : (
-                <div className="h2 p-3 noNews m-5">
-                    <p>No hay novedades por el momento</p>
-                </div>
-            )}
+          <div className='row my-3'>
+            {newsData.map((article) => (
+              <div className='col-sm-12 col-md-6 col-lg-4 justify-content-center'>
+                <NewsItem key={article.id} content={article} />
+              </div>
+            ))}
+          </div>
         </>
-    );
-}
- 
+      ) : (
+        <div className='row my-3 ml-5 pl-5'>
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+            <div className='col-sm-12 col-md-6 col-lg-4 justify-content-center'>
+              <Skeleton type='rectangular' width='250' height='300' />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
 export default NewsList;
