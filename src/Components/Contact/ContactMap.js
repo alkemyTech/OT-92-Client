fa480ead31f3847425d8a4053b9324b68b3deda7
fdelavalle <delavallef4@gmail.com>
2021-11-19 14:57:41 -0300

    added custom marker

--- src/Components/Contact/ContactMap.js
+++ src/Components/Contact/ContactMap.js
@@ -1,14 +1,33 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import { Popup, MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './contactMap.css';
+import L from 'leaflet';
 
-const ContactMap = (props) => {
+const ContactMap = ({ iconLink }) => {
+  const position = [-34.55881726737178, -58.47476996280374];
+
+  const markerIcon = L.icon({
+    //receives a link for the icon as a prop
+    iconUrl: iconLink
+      ? iconLink
+      : 'https://www.agroavisos.net/wp-content/uploads/2017/04/map-marker-icon.png',
+    iconSize: [40, 40],
+    iconAnchor: [12, 41],
+    popupAnchor: [8, -40],
+    shadowSize: [41, 41],
+  });
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <Marker position={[51.505, -0.09]}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={position} icon={markerIcon}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          <span>
+            <strong>SomosMas</strong>
+          </span>
         </Popup>
       </Marker>
     </MapContainer>
