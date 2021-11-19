e3b7b9236f9100b389e392442ac8e736a9ed1a05
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-18 22:41:31 -0300

    LazyLoad fixed

---
+++ src/Components/LazyLoadImages/LazyLoad.js
@@ -0,0 +1,11 @@
+import React,{Suspense} from 'react';
+import './LazyLoad.css';
+
+const LazyLoad = ({image}) => {
+
+    return ( 
+         <img style={{width:"100%", height:"380px"}} src={image} alt="" />
+     );
+}
+
+export default LazyLoad;
