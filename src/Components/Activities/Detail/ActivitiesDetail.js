22e3ed8c6fbca73e2a8cd98adf834e63fbffeb00
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-23 19:29:39 -0300

    ESLint fixed

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -49,22 +49,18 @@ const ActivitiesDetail = () => {
   return (
     <>
       <div>
-        {paramsActivity[0] ? (
-          <div className="container d-flex flex-column justify-items-center mt-5">
-            <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
-            {/* Anterior contenido sin mi componente  <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1> */}
-            <ActivitiesDetailContent 
-              body={paramsActivity[0].body}
-              paramsId={paramsId}
-            />
-          </div>
-        ) : (
-          <div className='mt-5 container'>
-            <h1 className='Activities-title '>
-              Oh Oh! hemos tenido problemas con esa actividad :({" "}
-            </h1>
-          </div>
-        )}
+        {loading ? <SpinnerCharge /> :
+          (
+            <div className="container d-flex flex-column justify-items-center mt-5">
+              <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
+              {/* Anterior contenido sin mi componente  <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1> */}
+              <ActivitiesDetailContent 
+                body={paramsActivity[0].body}
+                paramsId={paramsId}
+              />
+            </div>
+          )
+        }
       </div>
     </>
   );
