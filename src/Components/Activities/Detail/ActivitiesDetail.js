6f0bb21ba89a015e21d3492128e748e352099994
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-23 19:53:24 -0300

    State and http request using axios

--- src/Components/Activities/Detail/ActivitiesDetail.js
+++ src/Components/Activities/Detail/ActivitiesDetail.js
@@ -5,16 +5,25 @@ import { activitiesService } from "../../../Services/privateApiService";
 import ActivitiesDetailContent from "./ActivitiesDetailContent";
 import SpinnerCharge from "../../../Assets/SpinnerComponent/SpinnerCharge";
 import { errorAlert } from "../../../Assets/Alerts/alerts";
+import axios from "axios";
 
 const ActivitiesDetail = () => {
   let { id: paramsId } = useParams();
   const [loading, setLoading] = useState(false);
+  const [activity, setActivity] = useState({
+    id: null,
+    name: "",
+    description: "",
+    image: "",
+  });
 
   useEffect(() => {
     const getData = async () => {
       try{
         setLoading(true);
-        //Servicio reutilizable para traer el item by ID con await
+        const data = await axios.get(`http://ongapi.alkemy.org/public/api/activities/${paramsId}`);
+        console.log(data.data.data);
+        setActivity(data.data.data);
         setLoading(false);
       }
       catch{
@@ -32,10 +41,10 @@ const ActivitiesDetail = () => {
         {loading ? <SpinnerCharge /> :
           (
             <div className="container d-flex flex-column justify-items-center mt-5">
-              <h1 className=" h1 w-100 text-center Activities-title">{paramsActivity[0].title}</h1>
+              <h1 className=" h1 w-100 text-center Activities-title">{activity.name}</h1>
               {/* Anterior contenido sin mi componente  <h1 className="h3 Activities-body text-justify">{paramsActivity[0].body}</h1> */}
               <ActivitiesDetailContent 
-                body={paramsActivity[0].body}
+                body={activity.description}
                 paramsId={paramsId}
               />
             </div>
