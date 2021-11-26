88d17744b78d3909d5e75852aab1c0d8082710e3
fdelavalle <delavallef4@gmail.com>
2021-11-26 16:40:33 -0300

    added linkedin and twitter widgets to about component

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 import Title from "../Title";
+import {
+  LinkedinCompanyProfile,
+  LinkedinFollowCompany,
+  LinkedinLogin,
+  LinkedinAddProfile,
+  LinkedinProfile,
+  LinkedinShare,
+  TwitterButton,
+  TwitterTweet,
+} from "react-social-plugins";
 
 const About = () => {
-  const [text, setText] = useState("");
-
   return (
-    <div className='p-5'>
+    <div className="p-5">
       <Title content={{ title: "Nosotros" }} />
-      {!text ? (
-        <div class='d-flex justify-content-center mt-5'>
-          <div class='spinner-border'>
-            <span class='visually-hidden'>Loading...</span>
-          </div>
+
+      <div className="d-flex mt-5 justify-content-center">
+        <div
+          class="badge-base LI-profile-badge"
+          data-locale="es_ES"
+          data-size="medium"
+          data-theme="light"
+          data-type="VERTICAL"
+          data-vanity="somos-más-a1a817207"
+          data-version="v1"
+        ></div>
+
+        <div>
+          <TwitterTweet
+            tweetId="1451372239375540226"
+            theme="light"
+            width={325}
+          />
         </div>
-      ) : (
-        <p className='mt-5'>{text}</p>
-      )}
+      </div>
     </div>
   );
 };
