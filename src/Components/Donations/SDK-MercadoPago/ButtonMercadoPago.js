8d1b7b664e4338044878f153ce8c974228661af4
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-27 14:32:37 -0300

    input and button for donation component created

---
+++ src/Components/Donations/SDK-MercadoPago/ButtonMercadoPago.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Formik, Form } from "formik";
+
+const ButtonMercadoPago = () => {
+  
+  return (
+    <Formik
+    >
+      <Form id="form-container">
+      </Form>
+    </Formik>
+  );
+};
+
+export default ButtonMercadoPago;
