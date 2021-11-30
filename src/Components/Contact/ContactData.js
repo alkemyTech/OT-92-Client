6c7b582409c8594f89b8f01cbbe00602a084c28a
Manuel Gonzalez Mouriño <manuelgonzalez4a@gmail.com>
2021-11-29 22:36:32 -0300

    last change

--- src/Components/Contact/ContactData.js
+++ src/Components/Contact/ContactData.js
@@ -19,15 +19,7 @@ export default function datasList() {
           <div className="container">
             <div>
               <div className="card-body border rounded mb-3 mx-5 p-4">
-                <p>Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y 
-                                papás, abuelos y vecinos del barrio La Cava generando procesos de crecimiento y de 
-                                inserción social. Uniendo las manos de todas las familias, las que viven en el barrio
-                                y las que viven fuera de él, es que podemos pensar, crear y garantizar estos procesos.
-                                ﻿ Somos una asociación civil sin fines de lucro que se creó en 1997 con la intención de
-                                dar alimento a las familias del barrio. Con el tiempo fuimos involucrándonos con la
-                                comunidad y agrandando y mejorando nuestra capacidad de trabajo. Hoy somos un centro
-                                comunitario que acompaña a más de 700 personas a través de las áreas de: Educación,
-                                deportes, primera infancia, salud, alimentación y trabajo social.</p>
+                <p>En esta sección podrás encontrar un mapa con la ubicación de nuestra sede, asi como también un formulario para ponerte en contacto con nosotros.</p>
               </div>
             </div>
           </div>    
